8996a00fb6559643f4c49f4c5344f0ab557fa853
UnquietBones <35631425+UnquietBones@users.noreply.github.com>
2019-01-11 21:07:51 -0500

    Many many changed... but the actions work!
    I carved this thing up (and made it nice and messy with the beginnings of debugs) but the room actions are finally mostly working! I can pick things up, put them down, but actions are still in dire need of fixing. (woe)

--- Room.java
+++ Room.java
@@ -4,86 +4,206 @@ import java.util.Scanner;
 
 public class Room {
 
-    boolean showDebug;
-    boolean exitGame;
+    private boolean showDebug;
+    private ItemLibrary gameItems;
+    private ExitLibrary gameExits;
+    private ActionLibrary gameActions;
+    private ListOfThings playerInventory;
+    private RoomMap gameMap;
 
     public String ID;
     public String name;
     public String description;
-
     public ListOfThings items;
     public ListOfThings exits;
     public ListOfThings actions;
 
     private static Scanner reader = new Scanner(System.in);
 
-    // Right now we'll have a default max of 6 for everything, not sure if I can
-    // specified at time of room creation? Still researching.
-
-    public Room(String roomID, String roomName, String roomDescription, String[] roomItems, String[] roomExits, String[] roomActions, boolean mainDebug, boolean mainExit) {
+    public Room(String roomID, String roomName, String roomDescription, String[] roomItems, String[] roomExits,
+                String[] roomActions, ItemLibrary maingameItems, ExitLibrary maingameExits,
+                ActionLibrary maingameActions, ListOfThings mainplayerInventory, RoomMap maingameMap, boolean mainDebug) {
 
         showDebug = mainDebug;
-        exitGame = mainExit;
+        gameItems = maingameItems;
+        gameExits = maingameExits;
+        gameActions = maingameActions;
+        playerInventory = mainplayerInventory;
+        gameMap = maingameMap;
+
         ID = roomID;
         name = roomName;
         description = roomDescription;
 
-        items = new ListOfThings("Items", 6, showDebug);
-        exits = new ListOfThings("Exits", 6, showDebug);
-        actions = new ListOfThings("Actions", 6, showDebug);
+        // Right now we'll have a default max of 6 for everything, not sure if I can
+        // specified at time of room creation? Still researching.
 
-        items.itemsList = roomItems;
-        exits.itemsList = roomExits;
-        actions.itemsList = roomActions;
+        items = new ListOfThings("Room Inventory", "RoomInv", 6, gameItems, gameExits, gameActions, showDebug);
+        exits = new ListOfThings("Room Exits", "Exits", 6, gameItems, gameExits, gameActions, showDebug);
+        actions = new ListOfThings("Room Actions", "Actions", 6, gameItems, gameExits, gameActions, showDebug);
+
+        // Initialise the lists and create the items (if needed)
+
+        items.clearList();
+        exits.clearList();
+        actions.clearList();
+
+        if (showDebug) {
+            System.out.println("-------------------------");
+            System.out.println("Starting Room creation...");
+        }
+
+        if (showDebug) {
+            System.out.println("Creating Items...");
+        }
+
+        for (int itemPos = 0; itemPos < roomItems.length; itemPos++) {
+
+            if (showDebug) {
+                System.out.println("Checking [" + itemPos + "] " + roomItems[itemPos]);
+            }
+
+            if (roomItems[itemPos] != null && !roomItems[itemPos].isEmpty()) {
+                items.addItem(roomItems[itemPos]);
+            }
+        }
+
+        if (showDebug) {
+            System.out.println("-----------------");
+            System.out.println("Creating Exits...");
+        }
+
+        for (int exitPos = 0; exitPos < roomExits.length; exitPos++) {
+
+            if (showDebug) {
+                System.out.println("Checking [" + exitPos + "] " + roomExits[exitPos]);
+            }
+
+            if (roomExits[exitPos] != null && !roomExits[exitPos].isEmpty()) {
+                exits.addItem(roomExits[exitPos]);
+            }
+        }
+
+        if (showDebug) {
+            System.out.println("-------------------");
+            System.out.println("Creating Actions...");
+        }
+
+        for (int actionPos = 0; actionPos < roomActions.length; actionPos++) {
+
+            if (showDebug) {
+                System.out.println("Checking [" + actionPos + "] " + roomActions[actionPos]);
+            }
+
+            if (roomActions[actionPos] != null && !roomActions[actionPos].isEmpty()) {
+                actions.addItem(roomActions[actionPos]);
+            }
+        }
+        if (showDebug) {
+            System.out.println("-------------------------");
+        }
     }
 
-    public void printRoom(ListOfThings playerInventory, ItemLibrary gameItems) {
+
+    public void printRoom() {
+
+        if (showDebug) {
+            System.out.println("Starting Room printRoom...");
+        }
 
         System.out.println(name);
         System.out.println("---------------------------");
         System.out.println(description);
         System.out.println("");
-        items.printListOfThings();
-        exits.printListOfThings();
-        actions.printListOfThings();
-        playerInventory.printListOfThings();
+        items.printListOfThings("Pickup item", playerInventory);
+        exits.printListOfThings("Use Exit", playerInventory);
+        actions.printListOfThings("Do Action", playerInventory);
+        playerInventory.printListOfThings("Drop Inventory Item", playerInventory);
         System.out.println("---------------------------");
     }
 
-    public boolean roomAction(ListOfThings playerInventory, ItemLibrary gameItems) {
+    public boolean roomAction() {
+
+        if (showDebug) {
+            System.out.println("Starting Room roomAction...");
+        }
 
         String userInput;
+        int getFreeSpot;
 
         System.out.printf("> ");
         userInput = reader.nextLine();
 
+        if (userInput == null || userInput.isEmpty()) {
+            System.out.println("The universe listens, but does not respond.");
+            return false;
+        }
+
         if (items.isInList(userInput)) {
 
-            //If it's an Item name, try and pick it up
-            if (playerInventory.addItem(userInput)) {
-                items.dropItem(userInput);
+            getFreeSpot = playerInventory.freeSpot();
+
+            // If it's an Item name, try and pick it up
+            if (getFreeSpot == 999) {
+                System.out.println("You already have " + items.maxItems + ", you cannot carry anymore.");
+            } else {
+
+                // Move it from the room inventory to the player inventory
+                playerInventory.itemsList[getFreeSpot] = items.transferThing(userInput);
                 return true;
             }
         } else {
 
             // If it's an Exit, try and move there
-            if (exits.isInList(userInput)) {
-                // No idea how to do this yet
+            // using the exit will output the description and then change the current room
+
+            int exitPos = exits.posInList(userInput);
+            ListThing thisExit;
+
+            if (exitPos < 999) {
+
+                thisExit = exits.itemsList[exitPos];
+
+
+                thisExit = gameExits.readItem(userInput);
+                thisExit.useRoomExit(gameMap);
+
             } else {
 
                 // If it's an Action, perform the Action
-                if (actions.isInList(userInput)) {
+                if (actions.posInList(userInput) < 999) {
 
-                    gameItems.readItem(userInput, exitGame);
+                    gameItems.readItem(userInput);
 
-                // If it's not an Item, Exit, or Action... nothing happens.
                 } else {
-                    System.out.println("The universe listens, but does not respond.");
-                }
 
+                    // If it's an item in the player Inventory, try to drop it
+
+                    if (showDebug) {
+                        System.out.println("Trying to drop " + userInput);
+                    }
+
+                    if (playerInventory.posInList(userInput) < 999) {
+
+                        getFreeSpot = items.freeSpot();
+
+                        // If it's an Item name, try and pick it up
+                        if (getFreeSpot == 999) {
+                            System.out.println("The room already has " + items.maxItems + ", you cannot drop this here.");
+                        } else {
+
+                            // Move it from the room inventory to the player inventory
+                            items.itemsList[getFreeSpot] = playerInventory.transferThing(userInput);
+                            return true;
+                        }
+                    } else {
+
+                        // If it's not an Item, Exit, or Action... nothing happens.
+                        System.out.println("The universe listens, but does not respond.");
+                    }
+                }
             }
         }
-
         // If we've hit this point, assume no successful action was taken
         return false;
     }
