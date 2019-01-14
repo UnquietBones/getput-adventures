b42025e681fc917d463513591e0b48cb88912a80
UnquietBones <35631425+UnquietBones@users.noreply.github.com>
2019-01-13 20:55:04 -0500

    Added debugging and tweaked the code
    Added a bunch of debugging so I could track what it was doing easier and had to rework some of the code. Right now notes are horribly vague because I'm not really keeping track of what I'm fixing. (I'll do better after this! :) )

--- RoomMap.java
+++ RoomMap.java
@@ -1,6 +1,9 @@
 package getput.adventures;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.io.File;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class RoomMap {
@@ -65,46 +68,126 @@ public class RoomMap {
             }
 
             if (!roomIDs[roomCounter].isEmpty()) {
+
+                if (showDebug) {
+                    System.out.println("Reading in room name.");
+                }
                 roomNames[roomCounter] = scan.next();
+
+
+                if (showDebug) {
+                    System.out.println("Reading in room description.");
+                }
                 roomDescriptions[roomCounter] = scan.next();
 
                 // The next sections have comma delimitated subvalues
 
+                // -------------------------------------------------------------------------------------
+                if (showDebug) {
+                    System.out.println("--------------");
+                    System.out.println("Reading in room items.");
+                }
                 allItems = scan.next();
                 if (showDebug) {
                     System.out.println(allItems);
                 }
+
+                // First clear out all the values in the room's item list
+                for (int itemPos = 0; itemPos < maxItems; itemPos++) {
+                    roomItems[roomCounter][itemPos] = "";
+                }
+
+                // Then copy in the items (if there are any)
+
                 if (!allItems.equals("None")) {
-                    roomItems[roomCounter] = allItems.split(",");
-                    itemCount = roomItems[roomCounter].length;
+                    String[] tempItems = allItems.split(",");
+                    itemCount = tempItems.length;
+
                     if (showDebug) {
                         System.out.println(itemCount + " items found.");
                     }
+
+                    for (int newItemPos = 0; newItemPos < itemCount; newItemPos++) {
+                        roomItems[roomCounter][newItemPos] = tempItems[newItemPos];
+                    }
+                } else {
+                    if (showDebug) {
+                        System.out.println("No Items found.");
+                    }
                 }
 
+                // -------------------------------------------------------------------------------------
+                if (showDebug) {
+                    System.out.println("-----------");
+                    System.out.println("Reading in room exits.");
+                }
                 allExits = scan.next();
+                if (showDebug) {
+                    System.out.println(allExits);
+                }
+
+                // First clear out all the values in the room's exit list
+                for (int exitPos = 0; exitPos < maxItems; exitPos++) {
+                    roomExits[roomCounter][exitPos] = "";
+                }
+
+                // Then copy in the exits (if there are any)
+
                 if (!allExits.equals("None")) {
-                    if (showDebug) {
-                        System.out.println(allExits);
-                    }
-                    roomExits[roomCounter] = allExits.split(",");
-                    exitCount = roomExits[roomCounter].length;
+
+                    String[] tempExits = allExits.split(",");
+                    exitCount = tempExits.length;
+
                     if (showDebug) {
                         System.out.println(exitCount + " Exits found.");
                     }
+
+                    for (int newExitPos = 0; newExitPos < exitCount; newExitPos++) {
+                        roomExits[roomCounter][newExitPos] = tempExits[newExitPos];
+                    }
+                } else {
+                    if (showDebug) {
+                        System.out.println("No Exits found.");
+                    }
+                }
+
+                // -------------------------------------------------------------------------------------
+                if (showDebug) {
+                    System.out.println("-----------");
+                    System.out.println("Reading in room actions.");
                 }
 
                 allActions = scan.next();
+                if (showDebug) {
+                    System.out.println(allActions);
+                }
+
+                // First clear out all the values in the room's exit list
+                for (int actionPos = 0; actionPos < maxItems; actionPos++) {
+                    roomActions[roomCounter][actionPos] = "";
+                }
+
+                // Then copy in the actions (if there are any)
+
                 if (!allActions.equals("None")) {
-                    if (showDebug) {
-                        System.out.println(allActions);
-                    }
-                    roomActions[roomCounter] = allActions.split(",");
-                    actionCount = roomActions[roomCounter].length;
+
+                    String[] tempActions = allActions.split(",");
+                    actionCount = tempActions.length;
+
                     if (showDebug) {
                         System.out.println(actionCount + " Actions found.");
                     }
+
+                    for (int newActionPos = 0; newActionPos < actionCount; newActionPos++) {
+                        roomActions[roomCounter][newActionPos] = tempActions[newActionPos];
+                    }
+                } else {
+                    if (showDebug) {
+                        System.out.println("No Actions found.");
+                    }
                 }
+
+                // -------------------------------------------------------------------------------------
 
                 if (showDebug) {
                     System.out.println("Room ID          " + roomIDs[roomCounter]);
@@ -123,29 +206,12 @@ public class RoomMap {
             roomCounter += 1;
         }
         scan.close();
-
-        // Silly graphics just for fun!
-
-        System.out.println();
-        System.out.println("=========================================================");
-        System.out.println("===================|||||||||||===========================");
-        System.out.println("===================|||||||||||===========================");
-        System.out.println("===================|||||||||||===========================");
-        System.out.println("===================|||||||||||===========================");
-        System.out.println("=================|||||||||||||||=========================");
-        System.out.println("==================|||||||||||||==========================");
-        System.out.println("===================|||||||||||===========================");
-        System.out.println("====================|||||||||============================");
-        System.out.println("=====================|||||||=============================");
-        System.out.println("======================|||||==============================");
-        System.out.println("=======================|||===============================");
-        System.out.println("========================|================================");
-        System.out.println();
     }
 
     public Room readRoom(String findThisID) {
 
         if (showDebug) {
+            System.out.println("----------------------------");
             System.out.println("Starting RoomMap readRoom...");
         }
 
@@ -157,7 +223,7 @@ public class RoomMap {
         String[] thisRoomActions;
 
         for (int roomPos = 0; roomPos < roomIDs.length; roomPos++) {
-            if (roomIDs[roomPos].equals(findThisID)) {
+            if (roomIDs[roomPos].equals(findThisID) || roomNames[roomPos].equals(findThisID)) {
 
                 thisRoomName = roomNames[roomPos];
                 thisRoomDescription = roomDescriptions[roomPos];
@@ -168,16 +234,21 @@ public class RoomMap {
                 return thisRoom;
             }
         }
-        System.out.println("ERMAHGERD! Unable to find room ID " + findThisID + ", aborting game.");
+
+        if (showDebug) {
+            System.out.println("ERMAHGERD! Unable to find room ID " + findThisID + ".");
+            System.out.println("----------------------------");
+        }
         return thisRoom;
     }
 
     public void updateRoom(Room changedRoom) {
         // Setting it up so everything can change except the room ID
-        // Returns 1 if it updates the room, 0 if it can't find the room
 
         if (showDebug) {
+            System.out.println("------------------------------");
             System.out.println("Starting RoomMap updateRoom...");
+            System.out.println("Trying to update room " + changedRoom.ID + " " + changedRoom.name);
         }
 
         String oldValue;
@@ -185,27 +256,76 @@ public class RoomMap {
 
         for (int roomPos = 0; roomPos < roomIDs.length; roomPos++) {
             if (roomIDs[roomPos].equals(changedRoom.ID)) {
+
+                if (showDebug) {
+                    System.out.println("Updating room name from " + roomNames[roomPos] + " to " + changedRoom.name);
+                }
                 roomNames[roomPos] = changedRoom.name;
+
+                if (showDebug) {
+                    System.out.println("Updating room description from " + roomDescriptions[roomPos] + " to " + changedRoom.description);
+                }
                 roomDescriptions[roomPos] = changedRoom.description;
 
                 // Time to translate the ListOfThings back into Strings
 
-                for (int itemPos = 0; itemPos < 6; itemPos++) {
+                if (showDebug) {
+                    System.out.println("Updating room Items");
+                    System.out.println(roomItems[roomPos]);
+                    System.out.println(changedRoom.items);
+                    System.out.println("Expecting " + maxItems + " in Old Room, found " + roomItems[roomPos].length);
+                    System.out.println("Expecting " + maxItems + " in New Room, found " + changedRoom.items.itemsList.length);
+                }
 
-                    oldValue = roomItems[roomPos][itemPos];
-                    newValue = changedRoom.items.itemsList[itemPos].id;
+                for (int itemPos = 0; itemPos < maxItems; itemPos++) {
+
+                    if (showDebug) {
+                        System.out.println("Checking [" + itemPos + "]");
+                    }
+
+                    if (roomItems[roomPos][itemPos] == null) {
+
+                        if (showDebug) {
+                            System.out.println("The old item was empty.");
+                        }
+                        oldValue = "";
+                    } else {
+                        oldValue = roomItems[roomPos][itemPos];
+                        if (showDebug) {
+                            System.out.println("The old value was " + oldValue);
+                        }
+                    }
+
+                    if (changedRoom.items.itemsList[itemPos] == null) {
+                        if (showDebug) {
+                            System.out.println("The new item was empty.");
+                        }
+                        newValue = "";
+                    } else {
+                        newValue = changedRoom.items.itemsList[itemPos].id;
+                        if (showDebug) {
+                            System.out.println("The new value was " + newValue);
+                        }
+                    }
+
 
                     if (showDebug) {
                         System.out.println("Checking [" + itemPos + "] of Old " + oldValue + " and New " + newValue);
                     }
-
-                    if (newValue != null && !newValue.isEmpty()) {
-                        roomItems[roomPos][itemPos] = newValue;
-                    }
+                    roomItems[roomPos][itemPos] = newValue;
                 }
 
+                if (showDebug) {
+                    System.out.println("Updating room Exits");
+                    System.out.println(roomExits[roomPos]);
+                    System.out.println(changedRoom.exits);
+                }
 
-                for (int itemPos = 0; itemPos < 6; itemPos++) {
+                for (int itemPos = 0; itemPos < maxItems; itemPos++) {
+
+                    if (showDebug) {
+                        System.out.println("Checking [" + itemPos + "]");
+                    }
 
                     oldValue = roomExits[roomPos][itemPos];
                     newValue = changedRoom.exits.itemsList[itemPos].id;
@@ -214,12 +334,14 @@ public class RoomMap {
                         System.out.println("Checking [" + itemPos + "] of Old " + oldValue + " and New " + newValue);
                     }
 
-                    if (newValue != null && !newValue.isEmpty()) {
-                        roomExits[roomPos][itemPos] = newValue;
-                    }
+                    roomExits[roomPos][itemPos] = newValue;
                 }
 
-                for (int itemPos = 0; itemPos < 6; itemPos++) {
+                if (showDebug) {
+                    System.out.println("Updating room Actions");
+                }
+
+                for (int itemPos = 0; itemPos < maxItems; itemPos++) {
 
                     oldValue = roomActions[roomPos][itemPos];
                     newValue = changedRoom.actions.itemsList[itemPos].id;
@@ -228,12 +350,15 @@ public class RoomMap {
                         System.out.println("Checking [" + itemPos + "] of Old " + oldValue + " and New " + newValue);
                     }
 
-                    if (newValue != null && !newValue.isEmpty()) {
-                        roomActions[roomPos][itemPos] = newValue;
-                    }
+                    roomActions[roomPos][itemPos] = newValue;
                 }
+
+                if (showDebug) {
+                    System.out.println("Completed room update.");
+                }
+                return;
             }
         }
-        System.out.println("ERMAHGERD! Unable to update room " + changedRoom.name + ", aborting game.");
+        System.out.println("ERMAHGERD! Unable to update room " + changedRoom.name + ".");
     }
 }
