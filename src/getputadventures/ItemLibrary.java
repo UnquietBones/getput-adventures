bb8cc69ddb769bad69a99c2e8a84670b31b60f17
UnquietBones <35631425+UnquietBones@users.noreply.github.com>
2019-02-07 21:53:47 -0500

    Some comments and debug cleanup Cleaned up some comments and changed some of the debug output Swapped things over to use the new DebugMsgs Changed loadLibrary to loadItemLibrary Changed readThing to readItem

--- src/getputadventures/ItemLibrary.java
+++ src/getputadventures/ItemLibrary.java
@@ -14,17 +14,19 @@ public class ItemLibrary {
     private String[] itemActions = new String[libraryCount];
     private String[] itemPickup = new String[libraryCount];
     private Boolean showDebug;
+    private DebugMsgs debugMessage;
 
     public ItemLibrary(Boolean mainDebug) {
         showDebug = mainDebug;
+        debugMessage = new DebugMsgs(showDebug);
     }
 
-    public void loadLibrary() throws Exception {
-
-        debugLong();
-        debugOutput("ItemLibrary loadLibrary");
+    public void loadItemLibrary() throws Exception {
 
         // This program will read in the Library from the text file
+
+        debugMessage.debugLong();
+        debugMessage.debugOutput("ItemLibrary loadLibrary");
 
         int itemPos = 0;
 
@@ -33,15 +35,15 @@ public class ItemLibrary {
         Scanner scan = new Scanner(file);
         scan.useDelimiter("/|\\r\\n");
 
-        debugOutput("  Loading Item Library from " + pathname);
-        debugShort();
+        debugMessage.debugOutput("  Loading Item Library from " + pathname);
+        debugMessage.debugShort();
 
         // throw out the first line, it's the field descriptions for the end user
         scan.nextLine();
 
         while (scan.hasNext()) {
             itemIDs[itemPos] = scan.next();
-            debugOutput("  Checking line " + itemPos);
+            debugMessage.debugOutput("  Checking line " + itemPos);
 
             if (!itemIDs[itemPos].isEmpty()) {
                 itemNames[itemPos] = scan.next();
@@ -49,28 +51,28 @@ public class ItemLibrary {
                 itemActions[itemPos] = scan.next();
                 itemPickup[itemPos] = scan.next();
 
-                debugOutput("    Item ID          " + itemIDs[itemPos]);
-                debugOutput("    Item Name        " + itemNames[itemPos]);
-                debugOutput("    Item Description " + itemDescriptions[itemPos]);
-                debugOutput("    Item Action      " + itemActions[itemPos]);
-                debugOutput("    Item Pickup      " + itemPickup[itemPos]);
-                debugShort();
+                debugMessage.debugOutput("    Item ID          " + itemIDs[itemPos]);
+                debugMessage.debugOutput("    Item Name        " + itemNames[itemPos]);
+                debugMessage.debugOutput("    Item Description " + itemDescriptions[itemPos]);
+                debugMessage.debugOutput("    Item Action      " + itemActions[itemPos]);
+                debugMessage.debugOutput("    Item Pickup      " + itemPickup[itemPos]);
+                debugMessage.debugShort();
             } else {
-                debugOutput("  All done!");
+                debugMessage.debugOutput("  All done!");
             }
             itemPos += 1;
         }
         scan.close();
-        debugLong();
+        debugMessage.debugLong();
     }
 
-    public ListThing readThing(String findThing) {
+    public ListThing readItem(String findThing) {
 
         // This will find the item in the Library and then create a new ListThing with that information.
 
-        debugLong();
-        debugOutput("ItemLibrary readThing");
-        debugOutput("  Looking for Exit " + findThing);
+        debugMessage.debugLong();
+        debugMessage.debugOutput("ItemLibrary readItem");
+        debugMessage.debugOutput("  Looking for Item " + findThing);
 
         ListThing thisItem = new ListThing("Item", "", "", "", "",
                 "", "", "", showDebug);
@@ -97,19 +99,19 @@ public class ItemLibrary {
             thisActionID = itemActions[libraryPos];
             thisPickup = itemPickup[libraryPos];
 
-            debugOutput("  Creating item...");
-            debugOutput("    Item ID          " + thisID);
-            debugOutput("    Item Name        " + thisName);
-            debugOutput("    Item Description " + thisDescription);
-            debugOutput("    Item Action      " + thisActionID);
-            debugOutput("    Item Pickup      " + thisPickup);
-            debugLong();
+            debugMessage.debugOutput("  Creating item...");
+            debugMessage.debugOutput("    Item ID          " + thisID);
+            debugMessage.debugOutput("    Item Name        " + thisName);
+            debugMessage.debugOutput("    Item Description " + thisDescription);
+            debugMessage.debugOutput("    Item Action      " + thisActionID);
+            debugMessage.debugOutput("    Item Pickup      " + thisPickup);
+            debugMessage.debugLong();
 
             thisItem = new ListThing(thisType, thisID, thisName, thisDescription, thisActionID, thisCommand, thisActionType, thisPickup, showDebug);
             return thisItem;
         } else {
-            debugOutput("Unable to find Item " + findThing);
-            debugLong();
+            debugMessage.debugOutput("Unable to find Item " + findThing);
+            debugMessage.debugLong();
             return thisItem;
         }
     }
@@ -118,40 +120,32 @@ public class ItemLibrary {
         // This will find the location the Library of the Item or it will return 999
         // Checks for matches on ID or Name for simplicity's sake
 
-        debugLong();
-        debugOutput("ItemLibrary isInLibrary");
-        debugOutput("  Trying to find " + findThing);
+        debugMessage.debugLong();
+        debugMessage.debugOutput("ItemLibrary isInLibrary");
 
         for (int itemPos = 0; itemPos < itemIDs.length; itemPos++) {
-            debugOutput("Checking [" + itemPos + "] " + itemIDs[itemPos] + " " + itemNames[itemPos] + ".");
+            // debugMessage.debugOutput("Checking [" + itemPos + "] " + itemIDs[itemPos] + " " + itemNames[itemPos] + ".");
 
             if (itemIDs[itemPos].equalsIgnoreCase(findThing) || itemNames[itemPos].equalsIgnoreCase(findThing)) {
-                debugOutput("Item " + findThing + " was in the Item Library at [" + itemPos + "].");
-                debugLong();
+                debugMessage.debugOutput("Item " + findThing + " was in the Item Library at [" + itemPos + "] " + itemNames[itemPos]);
+                debugMessage.debugLong();
                 return itemPos;
             }
         }
 
-        debugOutput("Item " + findThing + " was not in the Item Library.");
-        debugLong();
+        debugMessage.debugOutput("Item " + findThing + " was not in the Item Library.");
+        debugMessage.debugLong();
         return 999;
     }
 
     public String findID(String findThing) {
         // Given the Name or ID for the Item, it will return the ID from the Library
 
-        debugLong();
-        debugOutput("ItemLibrary findID");
-
         int itemPos = this.isInLibrary(findThing);
 
         if (itemPos < 999) {
-            debugOutput("Found Item " + findThing + ".");
-            debugLong();
             return itemIDs[itemPos];
         } else {
-            debugOutput("Item " + findThing + " is not in the Library.");
-            debugLong();
             return "";
         }
     }
@@ -161,28 +155,28 @@ public class ItemLibrary {
         // Items can only have one action at a time, so removing the action will just
         // clear the value.
 
-        debugLong();
-        debugOutput("ItemLibrary removeItemAction");
-        debugOutput("  Trying to remove Action from Item " + updateID);
+        debugMessage.debugLong();
+        debugMessage.debugOutput("ItemLibrary removeItemAction");
+        debugMessage.debugOutput("  Trying to remove Action from Item " + updateID);
 
         int foundPos = this.isInLibrary(updateID);
 
         if (foundPos < 999) {
             this.itemActions[foundPos] = "";
-            debugOutput("  Removed Action from Item " + updateID);
-            debugLong();
+            debugMessage.debugOutput("  Removed Action from Item " + updateID);
+            debugMessage.debugLong();
         } else {
-            debugOutput("  Unable to find Item " + updateID + " in Library.");
-            debugLong();
+            debugMessage.debugOutput("  Unable to find Item " + updateID + " in Library.");
+            debugMessage.debugLong();
         }
     }
 
     public void addItemAction(String updateID, String actionID, ActionLibrary gameActions) {
         // Items can only have one action at a time, so adding the action will just replace whatever value is there.
 
-        debugLong();
-        debugOutput("ItemLibrary removeItemAction");
-        debugOutput("  Trying to update Item " + updateID + " with Action " + actionID);
+        debugMessage.debugLong();
+        debugMessage.debugOutput("ItemLibrary removeItemAction");
+        debugMessage.debugOutput("  Trying to update Item " + updateID + " with Action " + actionID);
 
         // Validate the Action ID
         actionID = gameActions.findID(actionID);
@@ -192,33 +186,15 @@ public class ItemLibrary {
 
             if (foundPos < 999) {
                 this.itemActions[foundPos] = actionID;
-                debugOutput("  Action " + actionID + " was added to the Item " + updateID + ".");
-                debugLong();
+                debugMessage.debugOutput("  Action " + actionID + " was added to the Item " + updateID + ".");
+                debugMessage.debugLong();
             } else {
-                debugOutput("  Unable to find Item " + updateID + " in Library.");
-                debugLong();
+                debugMessage.debugOutput("  Unable to find Item " + updateID + " in Library.");
+                debugMessage.debugLong();
             }
         } else {
-            debugOutput("  Action " + actionID + " is not in the Library, can't update Item " + updateID);
-            debugLong();
-        }
-    }
-
-    private void debugLong() {
-        if (showDebug) {
-            System.out.println("--------------------------------");
-        }
-    }
-
-    private void debugShort() {
-        if (showDebug) {
-            System.out.println("----------------");
-        }
-    }
-
-    private void debugOutput(String outputThis) {
-        if (showDebug) {
-            System.out.println(outputThis);
+            debugMessage.debugOutput("  Action " + actionID + " is not in the Library, can't update Item " + updateID);
+            debugMessage.debugLong();
         }
     }
 }
